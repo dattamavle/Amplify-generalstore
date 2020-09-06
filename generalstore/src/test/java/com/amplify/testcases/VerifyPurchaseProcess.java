f7820d5bb8582bc4e8c0b4c095f043250b6ffa48
Datta Mavle <datta.mawale74@gmail.com>
2020-09-06 08:51:38 +0530

    Base class created, configuration read from properties file and app launch process is done.
    1. Created Base class which take values from config.properties
    
    2. Written initialisation method to launch application

---
+++ generalstore/src/test/java/com/amplify/testcases/VerifyPurchaseProcess.java
@@ -0,0 +1,21 @@
+package com.amplify.testcases;
+
+import java.net.MalformedURLException;
+
+import org.testng.annotations.Test;
+
+import com.amplify.TestBase.TestBase;
+
+public class VerifyPurchaseProcess extends TestBase {
+	
+	public VerifyPurchaseProcess() {
+		super();
+	}
+	
+	@Test
+	public void toverifyPurchaseProduct() throws MalformedURLException
+	{
+		initialisation();
+	}
+
+}
